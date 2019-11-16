c4b081317573c6465a915a11fbeaa0a109fcd5d4
Farid El-Aouadi <faridelaouadi@hotmail.co.uk>
2019-11-16 14:44:58 +0000

    cleaned up the code and added comments

--- gif.js
+++ gif.js
@@ -1,7 +1,7 @@
-//setup before functions
-var typingTimer;                //timer identifier
-var doneTypingInterval = 500;  //time in ms, 5 second for example
-var $input = $('#text');
+
+var typingTimer; //timer identifier
+var doneTypingInterval = 500;  //time in ms, 0.5 second for example
+var $input = $('#text'); //get the text from the modal
 var image_counter = 0;  // we will use this to know which image is currently clicked
 
 //on keyup, start the countdown
@@ -10,17 +10,19 @@ $input.on('keyup', function () {
   if($(this).val()!=""){
     typingTimer = setTimeout(searchGifs, doneTypingInterval);
   }else{
-    console.log("The user cleared the input field and wants to search for something else")
+    //The user cleared the input field and wants to search for something else
+    image_counter = 0;
     document.getElementById("gifModalBody").innerHTML = "";
   }
 });
 
 //on keydown, clear the countdown
 $input.on('keydown', function () {
-  const key = event.key; // const {key} = event; ES6+
+  const key = event.key;
     if (key === "Backspace" || key === "Delete") {
         document.getElementById("gifModalBody").innerHTML = "";
     }
+    //if they erase something, we clear the current gifs
   clearTimeout(typingTimer);
 });
 
@@ -29,27 +31,18 @@ function searchGifs(){
   var request = new XMLHttpRequest()
 
   // Open a new connection, using the GET request on the URL endpoint
-  var query = document.getElementById("text").value
+  var query = document.getElementById("text").value //the user wants gifs for ...
   request.open('GET', 'https://api.giphy.com/v1/gifs/search?api_key=ZsDhKYMNVtzRxcbGwrOBKhNElDE4ZRDv&q='+query+'&limit=25&offset='+image_counter+'&rating=G&lang=en', true)
 
   request.onload = function() {
     // Begin accessing JSON data here
     var data = JSON.parse(this.response);
     if (request.status >= 200 && request.status < 400) {
-      //console.log(data["data"][0]["images"]["hd"]["mp4"])
       loadImages(data); //load the images into the modal body
-      //show modal
       $("#gifModal").modal(); //show the modal
-
-      // Fetch variables
-      /*while ($("#gifModal").modal()){
-        var scrollTop = $(gifModalBody).scrollTop(); // how far is the user down the content
-        console.log("the scrollTop is at " + scrollTop);
-      }*/
-
-
   }
 };
+
 // Send request
 request.send()
 };
@@ -59,20 +52,19 @@ function loadImages(data){
 
   var image_clicked;
   data["data"].forEach(value => {
+    //loop through all the GIF datasets
     var img = document.createElement('img'); //make the image element
     img.setAttribute('data-image_number', image_counter) //add a data attribute to it
     img.src =  value["images"]["fixed_height_downsampled"]["url"]; //get the image source from API response
     img.style.margin = "5px"; // styling
     img.addEventListener("click", () => {
-      //change the border of the image
       if (image_clicked){
         document.querySelector("img[data-image_number="+CSS.escape(image_clicked)+"]").style.border = 0;}
-        //unclick the previous image
+        //remove the border from the previously clicked image.
       image_clicked = img.getAttribute('data-image_number') //we need to know which image is clicked
       img.style.border = "4px solid #0000ff"; //create border around the image that was clicked
-      //console.log("you clicked the image with link ---> " + img.getAttribute('data-image_number'));
     });
-    document.getElementById("gifModalBody").appendChild(img);
+    document.getElementById("gifModalBody").appendChild(img); //add gif to the modal
     image_counter += 1;
   })
 
@@ -80,8 +72,7 @@ function loadImages(data){
 }
 
 function closeGifModal(){
-  document.getElementById("gifModalBody").innerHTML = "";
-  document.getElementById("text").value = "";
-  //this is the same as doing .innerHTML = '' however it is faster apparently
-  $('#gifModal').modal('toggle');
+  document.getElementById("gifModalBody").innerHTML = ""; //remove all the gifs from modal
+  document.getElementById("text").value = ""; // remove the user input from modal input field
+  $('#gifModal').modal('toggle'); //close the modal
 }
